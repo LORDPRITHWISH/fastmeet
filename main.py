750c7f4ea342168752d12c63ec4952dee91d19fb
LORD PRITHWISH <prithwishchakraborty05@gmail.com>
2024-08-31 19:49:29 +0530

    prisma

--- main.py
+++ main.py
@@ -3,7 +3,35 @@ import os
 from fastapi import FastAPI
 import uvicorn
 
+
+
 # export API_KEY=<YOUR_API_KEY>
+
+
+
+
+
+
+
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+
+
+
+
 
 
 
